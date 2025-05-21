f06acb380be152597fd4dab82d2d1fa0c2de64ab
Mohamed Aslam M <mohamedaslamcodes@gmail.com>
2025-05-21 15:26:37 +0530

    Custom Usage

--- main.go
+++ main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"gcommit/cmd"
 	"log"
 )
 
 func main() {
 	var push = flag.Bool("p", false, "Push changes to develop")
+	flag.Usage = func() {
+		fmt.Println("Never Again!")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
 	if err := cmd.Execute(*push); err != nil {
